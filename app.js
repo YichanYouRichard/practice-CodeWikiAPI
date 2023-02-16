80c77c7484f638100d9b0e9956555d3c5a89011f
Richard You <richardyou94@gmail.com>
2023-02-16 12:50:36 -0800

    get and post request work '

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -8,9 +9,57 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', function(req, res) {
-    res.send('Hello World! Server has created!')
-})
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+});
+
+const articleSchema = {
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", function (req, res) {
+  res.send("Hello World! Server has created!");
+});
+
+app.get("/articles", function (req, res) {
+  Article.find({}, function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function (req, res) {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const newArticle = new Article({
+    title: title,
+    content: content,
+  });
+  article.save(function (err) {
+    if (!err) {
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function (req, res) {
+    
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
